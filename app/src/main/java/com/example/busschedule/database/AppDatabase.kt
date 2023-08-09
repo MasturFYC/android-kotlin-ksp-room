8da28b1ede2d0484f9efe15d38d5407f46494468
MasturFyc <mastur.st12@gmail.com>
2023-08-09 19:24:19 +0700

    first commit

---
+++ app/src/main/java/com/example/busschedule/database/AppDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.busschedule.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Schedule::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun scheduleDao(): ScheduleDao
+
+    companion object {
+        @Volatile
+        private var Instance: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return Instance ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "app_database")
+                    .createFromAsset("database/bus_schedule.db")
+                    .build().also { Instance = it }
+                Instance = instance
+
+                instance
+            }
+        }
+
+    }
+
+}
